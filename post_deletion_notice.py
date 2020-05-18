5f6e42840d6f11e8d330513ec836fda8a638532a
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-05-18 15:31:17 +0530

    Update post_deletion_notice.py

--- post_deletion_notice.py
+++ post_deletion_notice.py
@@ -8,18 +8,102 @@ today = datetime.utcnow()
 
 post_del_file = ".logs/post_deletion_%s.csv" % today.strftime("%B_%Y")
 
-def Log_info(IsAware, FileName,UploaderName):
-    if not os.path.isfile(post_del_file):open(post_del_file, 'w').close()
-    with open(post_del_file,'a') as fd:
-        NewFileRow = "\n{c_a}, {c_b}, {c_c}".format(c_a=IsAware,c_b=FileName,c_c=UploaderName)
-        fd.write(NewFileRow)
+class DeletedFile:
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def deleter_admin(self):
+        for log in pywikibot.site.APISite.logevents(SITE,logtype="delete",page=self.file_name):
+            return log.data.get("user")
+    
+    def delete_comment(self):
+        for log in pywikibot.site.APISite.logevents(SITE,logtype="delete",page=self.file_name):
+            return log.data.get("comment")
 
-def AwarenessCheck(FileName,UploaderTalkPage):
-    if FileName in UploaderTalkPage.get():return "Yes"
-    else:return "No"
+    def uploader(self):
+        try:
+            return([info for info in pywikibot.site.APISite.logevents(SITE,logtype="upload",page=self.file_name,reverse=True,total=1)])[0].user()
+        except:
+            return "Unknown"
 
-def uploader(FileName):
-    return([info for info in pywikibot.site.APISite.logevents(SITE,logtype="upload",page=FileName,reverse=True,total=1)])[0].user()
+    def uploader_rights_list(self):
+        return pywikibot.User(SITE, self.uploader()).groups(force=True)
+
+    def uploader_talk_page(self):
+        user = pywikibot.User(SITE, self.uploader())
+        uploader_talk_page = user.getUserTalkPage()
+        if uploader_talk_page.isRedirectPage():
+            uploader_talk_page = uploader_talk_page.getRedirectTarget()
+        return uploader_talk_page
+
+    def is_aware(self):
+        if self.file_name in self.uploader_talk_page().get():return "Yes"
+        else:return "No"
+
+    def log_it(self):
+        if not os.path.isfile(post_del_file):open(post_del_file, 'w').close()
+        with open(post_del_file,'a') as fd:
+            NewFileRow = "\n{c_a}, {c_b}, {c_c}".format(c_a=self.is_aware(),c_b=self.file_name,c_c=self.uploader())
+            fd.write(NewFileRow)
+
+    def out_file_info(self):
+        out("Name : %s" % self.file_name, color = "yellow")
+        out("Uploader : %s" % self.uploader(), color = "yellow")
+        out("Deleted by : %s" % self.deleter_admin(), color = "yellow")
+        out("Delete reason : %s" % self.delete_comment(), color = "yellow")
+        out("\n\n")
+
+    def notify_uploader(self):
+        old_text = self.uploader_talk_page().get()
+        reason, admin = self.delete_comment(), self.deleter_admin()
+        new_text = ( old_text + "\n{{subst:User:Deletion Notification Bot/deleted notice|1=%s}}Deleted by [[User:%s]]. Reason for deletion : %s . \n~~~~" % (self.file_name, admin, reason))
+        summary = "[[%s]] was recently deleted by %s " % (self.file_name, admin)
+        commit(old_text, new_text, self.uploader_talk_page(), summary)
+
+def logged_data():
+    try:
+        with open(post_del_file, "r") as f:
+            post_del_data = f.read()
+    except:post_del_data = ""
+    Path(".logs").mkdir(parents=True, exist_ok=True)
+    m_log = ".logs/%s.csv" % today.strftime("%B_%Y")
+    if not os.path.isfile(m_log):open(m_log, 'w').close()
+    with open(m_log, "r") as f:
+        stored_data = f.read()
+    return (post_del_data + "\n" + stored_data)
+
+def Notify():
+    gen  = pagegenerators.LogeventsPageGenerator(
+        logtype = "delete",
+        site = SITE,namespace = 6,
+        start = pywikibot.site.APISite.getcurrenttimestamp(SITE),
+        end = (
+            today-timedelta(days=1)
+        ).strftime("%Y%m%d%H%M%S")
+    )
+    logged_files = logged_data()
+
+    for deleted_file in gen:
+        file_obj = DeletedFile(deleted_file.title())
+        if file_obj.file_name in logged_files:
+            continue
+        else:
+            file_obj.log_it()
+        try:
+            pywikibot.Page(SITE, file_obj.file_name).get(get_redirect=True, force=True)
+            continue
+        except:
+            Uploader = file_obj.uploader()
+            if Uploader == "Unknown":
+                continue
+            if 'bot' in file_obj.uploader_rights_list() or 'bot' in Uploader.lower():
+                out("We don't want the bot to notify another bot", color="white")
+                continue
+            file_obj.out_file_info()
+            if file_obj.is_aware() == "No":
+                file_obj.notify_uploader()
+            else:
+                continue
 
 def commit(old_text, new_text, page, summary):
     out("\nAbout to make changes at : '%s'" % page.title())
@@ -36,73 +120,6 @@ def out(text, newline=True, date=False, color=None):
     )
     pywikibot.stdout("%s%s" % (dstr, text), newline=newline)
 
-def deletion_info(FileName):
-    for log in pywikibot.site.APISite.logevents(SITE,logtype="delete",page=FileName):
-        reason = log.data.get("comment")
-        admin = log.data.get("user")
-    return reason, admin
-
-def post_data():
-    try:
-        with open(post_del_file, "r") as f:
-            post_del_data = f.read()
-    except:
-        post_del_data = ""
-    
-    return post_del_data
-
-def Notify():
-    gen  = pagegenerators.LogeventsPageGenerator(
-        logtype = "delete",
-        site = SITE,
-        namespace = 6,
-        start = pywikibot.site.APISite.getcurrenttimestamp(SITE),
-        end = (today-timedelta(days=1)).strftime("%Y%m%d%H%M%S"),
-        )
-    post_del_data = post_data()
-    Path(".logs").mkdir(parents=True, exist_ok=True)
-    m_log = ".logs/%s.csv" % today.strftime("%B_%Y")
-    if not os.path.isfile(m_log):open(m_log, 'w').close()
-    with open(m_log, "r") as f:
-        stored_data = f.read()
-
-    for deleted_file in gen:
-        FileName = deleted_file.title()
-
-        if FileName in (stored_data + "\n" + post_del_data):
-            out("%s was processed by the pre script." % FileName, color="white")
-            continue
-        
-        try:
-            old_text = pywikibot.Page(SITE, FileName).get(get_redirect=True, force=True)
-            continue
-        except:
-            pass
-
-            try:
-                Uploader = uploader(FileName)
-            except IndexError:
-                continue
-
-            rights_array = pywikibot.User(SITE,Uploader).groups(force=True)
-            if 'bot' in rights_array or 'bot' in Uploader.lower():
-                out("We don't want the bot to notify another bot", color="white")
-                continue
-            user = pywikibot.User(SITE, Uploader)
-            uploader_talk_page = user.getUserTalkPage()
-            if uploader_talk_page.isRedirectPage():
-                uploader_talk_page = uploader_talk_page.getRedirectTarget()
-            IsAware = AwarenessCheck(FileName,uploader_talk_page)
-            Log_info(IsAware,user.title(),FileName,)
-            out("""%s is deleted, it was uploaded by %s and they were %s of it's Deletion.""" % (FileName,user.title(), ("aware" if IsAware == "Yes" else "not aware")),)
-            if IsAware == "No":
-                old_text = uploader_talk_page.get()
-                reason, admin = deletion_info(FileName)
-                new_text = ( old_text + "\n{{subst:User:Deletion Notification Bot/deleted notice|1=%s}}Deleted by [[User:%s]]. Reason for deletion : %s . \n~~~~" % (FileName, admin, reason))
-                summary = "Notify user about deletion of [[%s]]" % FileName
-                commit(old_text, new_text, user.getUserTalkPage(), summary)
-            else:
-                continue
 def main():
     global SITE
     SITE = pywikibot.Site()
